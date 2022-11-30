98b2976b89dd2a00525f19a803af506fec47ee3b
vahap iren <vahapiren@gmail.com>
2022-11-30 15:49:58 +0300

    day37: Inheritance

---
+++ src/day37_Inheritance/animalTask/AnimalObject.java
@@ -0,0 +1,11 @@
+package day37_Inheritance.animalTask;
+
+public class AnimalObject {
+    public static void main(String[] args) {
+
+        Dog dog1=new Dog("Alex","Husky",'M',1,"Small","White");
+        System.out.println(dog1);
+
+
+    }
+}
--- src/day37_Inheritance/animalTask/Dog.java
+++ src/day37_Inheritance/animalTask/Dog.java
@@ -4,9 +4,8 @@ public class Dog extends Animal{
 
     public int a;
 
-    public Dog(String name, String breed, char gender, int age, String size, String color,int a){
+    public Dog(String name, String breed, char gender, int age, String size, String color){
         super(name, breed, gender, age, size, color);
-        this.a=a;
     }
 
     public void bark(){
