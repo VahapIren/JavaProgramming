c208029c86a0fe2a2327c47664fc87eabe708fc4
vahap iren <vahapiren@gmail.com>
2022-11-30 21:23:58 +0300

    day36: Inheritance

--- src/officeHours/NestedLoopsPractice.java
+++ src/officeHours/NestedLoopsPractice.java
@@ -1,10 +1,32 @@
 package officeHours;
 
+import java.util.Scanner;
+
+import static officeHours.WhilePractices.three;
+
 public class NestedLoopsPractice {
     public static void main(String[] args) {
 
-one();
+//one();
+//two();
+three();
+    }
 
+    private static void two() {
+        Scanner scan=new Scanner(System.in);
+        int number=scan.nextInt();
+        int total=0;
+        for (int i = 0; i <= number; i++) {
+            for (int j = 0; j <=number; j++) {
+                for (int k = 0; k <= number; k++) {
+                    if (i+j+k==number){
+                        System.out.println(""+i+j+k);
+                        total++;
+                    }
+                }
+            }
+        }
+        System.out.println(total);
     }
 
     public static void one(){
@@ -15,5 +37,14 @@ one();
             System.out.println();
         }
 
+    }
+
+    public static void three(){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
