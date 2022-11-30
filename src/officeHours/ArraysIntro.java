806f8e63fe18f0398a8917832ad80c629a58b26c
vahap iren <vahapiren@gmail.com>
2022-11-30 21:46:27 +0300

    officeHours

---
+++ src/officeHours/ArraysIntro.java
@@ -0,0 +1,24 @@
+package officeHours;
+
+public class ArraysIntro {
+    public static void main(String[] args) {
+
+one();
+    }
+    public static void one(){
+        int[] numbers={1,2,3,4,5,6,7,89,9,10};
+        int even=0,odd=0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (i%2==0)
+                even+=numbers[i];
+            else
+                odd+=numbers[i];
+        }
+        System.out.println(even);
+        System.out.println(odd);
+    }
+
+
+
+
+}
