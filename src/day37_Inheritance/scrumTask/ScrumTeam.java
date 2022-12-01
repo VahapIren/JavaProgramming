63dee50e5f3a2ba44c566a16823f145309cb4b68
vahap iren <vahapiren@gmail.com>
2022-12-01 12:06:17 +0300

    day 37: Inheritance

---
+++ src/day37_Inheritance/scrumTask/ScrumTeam.java
@@ -0,0 +1,41 @@
+package day37_Inheritance.scrumTask;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ScrumTeam {
+
+    public ProductOwner PO;
+    public BusinessAnalyst BA;
+    public ScrumMaster SM;
+
+    public ArrayList<Tester> testers=new ArrayList<>();
+    public ArrayList<Developer> developers=new ArrayList<>();
+
+    public ScrumTeam(ProductOwner PO, BusinessAnalyst BA, ScrumMaster SM) {
+        this.PO = PO;
+        this.BA = BA;
+        this.SM = SM;
+    }
+
+    public void addTester(Tester tester){
+        testers.add(tester);
+    }
+    public void addTesters(Tester[] testers){   //same name because of that we use "this"
+        this.testers.addAll(Arrays.asList(testers));
+    }
+    public void removeTester(int id){
+        testers.removeIf(p->p.id==id);
+    }
+    public void addDeveloper( Developer developer){
+        developers.add(developer);
+    }
+    public void addDevelopers(Developer[] developers){
+        this.developers.addAll(Arrays.asList(developers));
+    }
+    public void removeDeveloper(int id){
+        developers.removeIf(p->p.id==id);
+    }
+
+
+}
