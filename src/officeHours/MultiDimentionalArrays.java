9cfeab4bff2d98077b95767f4e33de2d0a3ea005
vahap iren <vahapiren@gmail.com>
2022-12-01 08:53:24 +0300

    officeHours

--- src/officeHours/MultiDimentionalArrays.java
+++ src/officeHours/MultiDimentionalArrays.java
@@ -1,11 +1,24 @@
 package officeHours;
 
+import java.util.Arrays;
+
 public class MultiDimentionalArrays {
     public static void main(String[] args) {
 
         int[] numbers=new int[5];
         int[][] numbers2D=new int[5][];
         int[][][] numbers3D=new int[5][][];
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.deepToString(numbers2D));
 
+        numbers[0]=5;
+        numbers2D[0]=new int[]{1,2,3};
+        System.out.println(Arrays.deepToString(numbers2D));
+
+        int[] element1={1,2,3,4};
+        numbers2D[1]=element1;
+        System.out.println(Arrays.deepToString(numbers2D));
+
+        System.out.println(numbers2D[1][1]);
     }
 }
