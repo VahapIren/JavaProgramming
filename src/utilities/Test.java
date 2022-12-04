1346b66ed53347e4d1632c2c9d695a9f27b7a703
vahap iren <vahapiren@gmail.com>
2022-12-04 21:21:50 +0300

    day 36: Inheritance

---
+++ src/utilities/Test.java
@@ -0,0 +1,16 @@
+package utilities;
+
+public class Test {
+    public Test() {
+
+        System.out.println("Developer ");
+
+    }
+    public void Test(){
+        System.out.println("SDET ");
+    }
+
+    public static void main(String[] args) {
+        Test obj=new Test();
+    }
+}
