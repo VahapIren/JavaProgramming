17635ad78c03498ec509db1bc477efade25eebb2
vahap iren <vahapiren@gmail.com>
2022-12-14 08:26:00 +0300

    officeHours

---
+++ src/officeHours/week8/CustomMethods.java
@@ -0,0 +1,23 @@
+package officeHours.week8;
+
+public class CustomMethods {
+    public static void main(String[] args) {
+
+        int a=5;
+        System.out.println(a);
+
+        System.out.println("This is for manual testing");
+        System.out.println("This is for web app testing");
+        System.out.println("This is for api testing");
+        System.out.println("This is for data base testing");
+        webAppTest();
+
+    }
+    public static void manualTesting(){
+        System.out.println("This is for manual testing");
+    }
+    public static void webAppTest(){
+        System.out.println("This is for web app testing");
+    }
+
+}
